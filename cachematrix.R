c127ae04309afb97f85d54fbc7e88d993f0d6f2c
japadros <japadros@yahoo.com>
2014-04-26 11:59:00 -0500

    Coursera assignment for peer revision; cache in environments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function for Program Assignment 2, R Programming Course in Coursera
+## Matrix inversion, keeping previously calculated results as a cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the environment and the functions for
+## initialization of values.
 
 makeCacheMatrix <- function(x = matrix()) {
+
+  # Setting initial value to NULL to indicate nothing calculated yet
+  m <- NULL
+  # Setting initial value of the x member variable from the vector
+  # given as argument to the set member function.
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  # Retreival of the x member value
+  get <- function() x
+  # Setting the set and get functions for usage of the object
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  # Output of list object with 4 elements
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Function for calculation and for cache retreival
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+
+  # Retreival of the cached value from the function's environment
+  m <- x$getinv()
+  # If it is not null, return the value and exit function
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  # Otherwise, receive the value from the other environment and calculate
+  # inverse using solve
+  data <- x$get()
+  m <- solve(data, ...)
+  # Set the calculated value in the other environment
+  x$setinv(m)
+  # Return the result
+  m
+
 }
